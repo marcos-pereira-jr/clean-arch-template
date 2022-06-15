9e91e50a8b083b01ab6f0a52e6f2eebbd3d270f6
Marcos Pereira <marcos.ant.pereira.jr@gmail.com>
2022-06-15 19:59:52 -0300

    Implementado Validator

--- clean-arch-template-core/lib/src/main/java/clean/arch/template/core/domain/person/validator/CNPJValidator.java
+++ clean-arch-template-core/lib/src/main/java/clean/arch/template/core/domain/person/validator/CNPJValidator.java
@@ -4,7 +4,7 @@ import clean.arch.template.core.domain.person.exception.InvalidDocument;
 
 import java.util.regex.Pattern;
 
-public class CNPJValidator {
+public class CNPJValidator implements Validator{
     public CNPJValidator(){}
 
     public static final Pattern FORMATED = Pattern.compile("(\\d{2})[.](\\d{3})[.](\\d{3})/(\\d{4})-(\\d{2})");
---
+++ clean-arch-template-core/lib/src/main/java/clean/arch/template/core/domain/person/validator/Validator.java
@@ -0,0 +1,5 @@
+package clean.arch.template.core.domain.person.validator;
+
+public interface Validator {
+    public boolean assertValid(String value);
+}
